de6bc03c1dda348270ad70bd9ba41d3001473ec8
ErixXxYe <derstylervonmorgen@gmail.com>
2022-11-11 10:46:42 +0100

    HÜ_erledigt

--- HTML_JS/MiniRechner/MiniRechner.js
+++ HTML_JS/MiniRechner/MiniRechner.js
@@ -1,25 +1,26 @@
-function calculate(){
-    var A = parseInt(document.getElementById("number1").value)
-    var B = parseInt(document.getElementById("number2").value)
-    var operator = parseInt(document.getElementById("operator").value)
-    var output = 0;
-    alert(document.getElementById("number1").value.type)
-    switch(operator){
-        case 0:
-            output = A + B;
-            document.getElementById("output").innerHTML = output;
-            break;
-        case 1:
-            output = A - B;
-            document.getElementById("output").innerHTML = output;
-            break;
-        case 2:
-            output = A * B;
-            document.getElementById("output").innerHTML = output;
-            break;
-        case 3:
-            output = A / B;
-            document.getElementById("output").innerHTML = output;
-            break;
-    } 
+function calculate() {
+  var A = parseInt(document.getElementById("number1").value);
+  var B = parseInt(document.getElementById("number2").value);
+  var operator = parseInt(document.getElementById("operator").value);
+  var output = 0;
+
+  switch (operator) {
+    case 0:
+      output = A + B;
+      alert(output);
+      document.getElementById("output").innerHTML = output;
+      break;
+    case 1:
+      output = A - B;
+      document.getElementById("output").innerHTML = output;
+      break;
+    case 2:
+      output = A * B;
+      document.getElementById("output").innerHTML = output;
+      break;
+    case 3:
+      output = A / B;
+      document.getElementById("output").innerHTML = output;
+      break;
+  }
 }
